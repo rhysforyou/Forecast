a477667990959bbb70e00d281ea6e538b5021188
Rhys Powell <rhys@rpowell.me>
2022-06-14 09:43:32 +1000

    Initial Commit

---
+++ Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Rhys Powell on 14/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
